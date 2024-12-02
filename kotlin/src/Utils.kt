cc93e797aa9e017f936706fce9aa5d93ab0f8253
Teun <teun.willems@live.nl>
2024-12-02 21:32:32 +0100

    add kotlin

---
+++ kotlin/src/Utils.kt
@@ -0,0 +1,14 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Path("kotlin/src/$name.txt").readText().trim().lines()
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
