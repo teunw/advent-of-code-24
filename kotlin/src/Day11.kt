4c2dab877ccb6242c2199762ce15355bd0480bc1
Teun Willems <t31662@eon.com>
2024-12-11 20:03:00 +0100

    Day 11 start

---
+++ kotlin/src/Day11.kt
@@ -0,0 +1,34 @@
+val stoneMap = mutableMapOf<String, ULong>()
+
+fun blinkStone(stone: String, times: Int): ULong {
+    if (times == 0) return 1UL
+    val key = "$stone|$times"
+    val alreadyFound = stoneMap[key]
+    if (alreadyFound != null) return alreadyFound
+    val newValue = when {
+        stone == "0" -> return blinkStone("1", times - 1)
+        stone.length % 2 == 0 ->
+            blinkStone(stone.substring(0, stone.length / 2).toULong().toString(), times - 1) +
+                    blinkStone(stone.substring(stone.length / 2, stone.length).toULong().toString(), times - 1)
+        else -> blinkStone((stone.toULong() * 2024UL).toString(), times - 1)
+    }
+    stoneMap[key] = newValue
+    return newValue
+}
+fun main() {
+    fun part1(input: String, blinks: Int): ULong {
+        return input.trim()
+            .split(' ')
+            .sumOf { blinkStone(it, blinks) }
+    }
+
+    // Test if implementation meets criteria from the description, like:
+    check(part1(readInput("Day11_example"), 25) == 55312UL)
+//    check(part2(readInput("Day11_example")) == 6)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day11")
+    println("Part 1 = ${part1(input, 25)}")
+    println("Part 2 = ${part1(input, 75)}")
+//    println("Part 2 = ${part2(input)}")
+}
